2110edb8cf66efc60511ce6c7a4ab05a2e2ac83e
FFM-in <178662316+FFM-in@users.noreply.github.com>
2025-06-20 12:45:58 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import PageNotFound from "components/commons/PageNotFound";
 import ProductList from "components/ProductList/ProductList";
 import { Redirect, Route, Switch } from "react-router-dom";
+import routes from "routes";
 
 import Product from "./components/Product/Product";
 
 const App = () => (
   <Switch>
-    <Route exact component={ProductList} path="/products" />
-    <Route exact component={Product} path="/products/:slug" />
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Route exact component={Product} path={routes.products.show} />
     <Redirect exact from="/" to="/products" />
     <Route component={PageNotFound} path="*" />
   </Switch>
